16fa521db60d8f784d4b867292e10a9ea0a8a31d
SrKiller-lab <pavyelyo@gmail.com>
2025-05-15 17:37:50 -0600

    Update clases_parte2.2.py

--- clases_parte2.2.py
+++ clases_parte2.2.py
@@ -1,7 +1,8 @@
+#Gomez Lopez Sergio
 class armamento:
-    daño = 100
+    daño = 100 #primero se le dan el valor principal a los atributos
     acc_slots = 5
-melee = armamento()
+melee = armamento() #esta parte es para que muestre en la terminal el valor estatico y seguido de eso el valor modificado en esta parte del codigo, al igual que la zona de abajo
 print(melee.daño)
 melee.daño = 150
 print(melee.daño)
